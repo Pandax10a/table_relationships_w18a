171cb529227b5d0569b74e47e21347de36b65117
panda1 <blueblue0011@gmail.com>
2022-09-14 12:52:03 -0700

    filled blogger table

---
+++ queries.sql
@@ -0,0 +1,7 @@
+insert into blogger(username, password, name, created_at, email)
+values
+('person1', 'pw1', 'one', '2020-1-1', 'person1@anymail.com'),
+('person2', 'pw2', 'two', '2019-2-2', 'person2@anymail.com'),
+('person3', 'pw3', 'three', '2022-3-3', 'person3@anymail.com'),
+('person4', 'pw4', 'four', '2021-4-4', 'person4@anymail.com'),
+('person5', 'pw5', 'five', '2018-5-5', 'person5@anymail.com');
